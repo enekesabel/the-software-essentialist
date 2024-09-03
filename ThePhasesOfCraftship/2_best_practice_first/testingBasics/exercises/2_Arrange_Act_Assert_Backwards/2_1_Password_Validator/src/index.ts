01254e013c59bbc027714eeb6cc70c1975899a25
enekesabel <enekes.abel@gmail.com>
2024-09-03 17:58:54 +0200

    refactor: move the logic of combining result into ValidationResultImpl utility class

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -5,78 +5,66 @@ export type ValidationError = {
 
 export type ValidationResult = {
     result: boolean;
-    errors: ValidationError[];
+    errors: ReadonlyArray<ValidationError>;
 };
+
+class ValidationResultImpl implements ValidationResult {
+    readonly result: boolean;
+    readonly errors: ReadonlyArray<ValidationError>;
+
+    constructor(result: boolean, errors: ValidationError[] = []) {
+        this.result = result;
+        this.errors = errors;
+    }
+
+    combine(other: ValidationResultImpl): ValidationResultImpl {
+        const combinedErrors = [...this.errors, ...other.errors];
+        const allPass = this.result && other.result;
+
+        return new ValidationResultImpl(allPass, combinedErrors);
+    }
+}
 
 export class PasswordValidator {
     static Validate(password: string): ValidationResult {
-        
-        const lengthValidation = this.ValidateLength(password);
-        const digitValidation = this.ValidateDigit(password);
-        const upperCaseValidation = this.ValidateUpperCase(password);
-
-        return this.CombineResults(lengthValidation, digitValidation, upperCaseValidation);
+        return this.ValidateLength(password).
+        combine(this.ValidateDigit(password)).
+        combine(this.ValidateUpperCase(password));
     }
 
-    private static ValidateLength(password: string): ValidationResult {
+    private static ValidateLength(password: string): ValidationResultImpl {
         if (password.length < 5 || password.length > 15) {
-            return {
-                result: false,
-                errors: [
-                    {
-                        type: 'InvalidLengthError',
-                        message: 'Password must be between 5 and 15 characters long.'
-                    }
-                ]
-            };
+            return new ValidationResultImpl(false, [
+                {
+                    type: 'InvalidLengthError',
+                    message: 'Password must be between 5 and 15 characters long.'
+                }
+            ]);
         }
-        return { result: true, errors: [] };
+        return new ValidationResultImpl(true);
     }
 
-    private static ValidateDigit(password: string): ValidationResult {
+    private static ValidateDigit(password: string): ValidationResultImpl {
         if (!/\d/.test(password)) {
-            return {
-                result: false,
-                errors: [
-                    {
-                        type: 'NoDigitError',
-                        message: 'Password must contain at least one digit.'
-                    }
-                ]
-            };
+            return new ValidationResultImpl(false, [
+                {
+                    type: 'NoDigitError',
+                    message: 'Password must contain at least one digit.'
+                }
+            ]);
         }
-        return { result: true, errors: [] };
+        return new ValidationResultImpl(true);
     }
 
-    private static ValidateUpperCase(password: string): ValidationResult {
+    private static ValidateUpperCase(password: string): ValidationResultImpl {
         if (!/[A-Z]/.test(password)) {
-            return {
-                result: false,
-                errors: [
-                    {
-                        type: 'NoUpperCaseCharacterError',
-                        message: 'Password must contain at least one upper case letter.'
-                    }
-                ]
-            };
+            return new ValidationResultImpl(false, [
+                {
+                    type: 'NoUpperCaseCharacterError',
+                    message: 'Password must contain at least one upper case letter.'
+                }
+            ]);
         }
-        return { result: true, errors: [] };
-    }
-
-    private static CombineResults(...results: ValidationResult[]): ValidationResult {
-        const combinedErrors: ValidationError[] = [];
-        let allPass = true;
-
-        for (const result of results) {
-            if (!result.result) {
-                allPass = false;
-                combinedErrors.push(...result.errors);
-            }
-        }
-
-        return {
-            result: allPass,
-            errors: combinedErrors
-        };
+        return new ValidationResultImpl(true);
     }
 }
