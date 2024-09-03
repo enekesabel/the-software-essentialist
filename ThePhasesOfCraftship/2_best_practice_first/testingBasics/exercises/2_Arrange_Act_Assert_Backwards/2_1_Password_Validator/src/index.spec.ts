20b57a5779dc95236ae655893451301c3a006e7b
enekesabel <enekes.abel@gmail.com>
2024-09-03 15:49:10 +0200

    docs: list possible collaborators, responsibilities, examples

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,3 +1,30 @@
+/*
+
+COLLABORATORS
+
+- PasswordValidator
+- ValidationResponse
+
+RESPONSIBILITIES
+
+PasswordValidator:
+
+- validates password.
+  - Between 5 and 15 characters long. 
+      Ex.: "thePhysical1234567" returns a false-y response because of exceeding the 15 character length
+  - Contains at least one digit.
+      Ex.: "maxwellTheBe" returns a false-y response because of a lack of digits
+  - Contains at least one upper case letter.
+      Ex.: "maxwell1_c" returns a false-y response because of a lack of uppercase characters
+- constructs ValidationResponse.
+Ex.: "thePhysical1" returns a true response because the password matches all criteria
+
+ValidationResponse:
+
+- holds boolean result of the validation.
+- holds the list of errors.
+*/
+
 
 describe('password validator', () => {
 
