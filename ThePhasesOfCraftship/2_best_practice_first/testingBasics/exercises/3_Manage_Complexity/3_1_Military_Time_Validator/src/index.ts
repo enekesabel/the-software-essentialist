b1c4ba1d4cfbb5258913414d4c700c7b59a24066
enekesabel <enekes.abel@gmail.com>
2024-09-04 15:51:05 +0200

    refactor: extract startTime and hour utility functions

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,31 +1,36 @@
+import { time } from "console";
+
 const validateHour = (hour:string) => {
     const parsedHour = parseInt(hour);
     return !isNaN(parsedHour) && parsedHour < 24;
 }
 
-export const militaryTimeValidator = (time: string): boolean => {
-    const formatMatches =  time.length === 13 &&
-    time.indexOf(' - ') > 0 &&
-    time.lastIndexOf(':') === 10 &&
-    time.indexOf(':') === 2;
+const hour = (time: string) => time.slice(0, 2);
+const startTime = (militaryTime: string) => militaryTime.slice(0, 4);
+
+export const militaryTimeValidator = (militaryTime: string): boolean => {
+    const formatMatches =  militaryTime.length === 13 &&
+    militaryTime.indexOf(' - ') > 0 &&
+    militaryTime.lastIndexOf(':') === 10 &&
+    militaryTime.indexOf(':') === 2;
 
     if(!formatMatches){
         return false;
     }
 
-    if(!validateHour(time.slice(0, 2))){
+    if(!validateHour(hour(startTime(militaryTime)))){
         return false
     }
 
-    if(!validateHour(time.slice(8, 10))){
+    if(!validateHour(militaryTime.slice(8, 10))){
         return false
     }
 
-    if(parseInt(time.slice(3, 5)) > 59){
+    if(parseInt(militaryTime.slice(3, 5)) > 59){
         return false
     }
 
-    if(parseInt(time.slice(11, 13)) > 59){
+    if(parseInt(militaryTime.slice(11, 13)) > 59){
         return false
     }
 
