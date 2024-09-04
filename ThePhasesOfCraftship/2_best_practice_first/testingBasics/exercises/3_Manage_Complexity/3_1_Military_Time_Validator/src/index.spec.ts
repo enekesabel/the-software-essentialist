5c36996fee3e4d2934bbf70a7bbb644bdfd617e1
enekesabel <enekes.abel@gmail.com>
2024-09-04 16:05:14 +0200

    docs: remove comments used for planning

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,29 +1,3 @@
-/*
-
-RESPONSIBILITIES
-- validate time string
-  - must match the `HH:MM - HH:MM` template
-        "00:00 - 00:00" (yes)
-        "0:00 - 00:00" (no)
-        "00:0 - 00:00" (no)
-        "00:00 - 0:00" (no)
-        "00:00 - 00:0" (no)
-  - validate on start and end time:
-    - hours range: 0-23
-            "25:00 - 00:00" (no)
-            "00:00 - 25:00" (no)
-    - minutes range: 0-59
-            "00:60 - 00:00" (no)
-            "00:00 - 00:60" (no)
-- return `true` or `false` based on the validation outcome
-
-Assignment examples:
-"01:12 - 14:32" (yes)
-"25:00 - 12:23" (no)
-"22:00 - 23:12" (yes)
-
-*/
-
 import { militaryTimeValidator } from "."
 
 describe('military time validator', () => {
