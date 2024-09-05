995ae968bcae1ad57fa4b0e8123362f6491b9d42
enekesabel <enekes.abel@gmail.com>
2024-09-05 12:16:40 +0200

    feat: create BooleanCalculator with fake impl

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,16 @@
+import { BooleanCalculator } from ".";
 
 describe('boolean calculator', () => {
+
+    it('Should evaluate "TRUE" to true', ()=>{
+        // arrange
+        const expression = 'TRUE';
+
+        // act
+        const result = BooleanCalculator.Evaluate(expression);
+
+        // assert
+        expect(result).toBe(true);
+    })
 
 })
