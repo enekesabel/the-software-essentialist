f52af862e427db9766492b0a697fdf635a2b45b7
enekesabel <enekes.abel@gmail.com>
2024-09-05 18:44:29 +0200

    feat: parse "AND" - naive impl

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/ExpressionParser.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/ExpressionParser.spec.ts
@@ -6,7 +6,11 @@ describe('ExpressionParser', () => {
         { booleanStr: 'TRUE', expected: [true] },
         { booleanStr: 'FALSE', expected: [false] },
         { booleanStr: 'NOT TRUE', expected: ['NOT', true] },
-        { booleanStr: 'NOT FALSE', expected: ['NOT', false] }
+        { booleanStr: 'NOT FALSE', expected: ['NOT', false] },
+        { booleanStr: 'TRUE AND TRUE', expected: [true, 'AND', true] },
+        { booleanStr: 'TRUE AND FALSE', expected: [true, 'AND', false] },
+        { booleanStr: 'FALSE AND TRUE', expected: [false, 'AND', true] },
+        { booleanStr: 'FALSE AND FALSE', expected: [false, 'AND', false] }
     ])('Should correctly parse "$booleanStr"', ({ booleanStr, expected }) => {
         // act
         const expression = ExpressionParser.Parse(booleanStr);
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/ExpressionParser.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/ExpressionParser.ts
@@ -1,26 +1,33 @@
 type Operator = 'NOT' | 'AND' | 'OR';
-
 type Value = true | false;
-
-type Expression = (Value|Operator|Expression)[];
+type Expression = (Value | Operator | Expression)[];
 
 export class ExpressionParser {
     static Parse(booleanString: string): Expression {
         const words = booleanString.split(' ');
+        const expression: Expression = [];
 
-        if (words[0] === 'NOT') {
-            const value = this.Parse(words.slice(1).join(' '));
-            return ['NOT', ...value];
+        for (let i = 0; i < words.length; i++) {
+            const word = words[i];
+
+            switch (word) {
+                case 'TRUE':
+                    expression.push(true);
+                    break;
+                case 'FALSE':
+                    expression.push(false);
+                    break;
+                case 'NOT':
+                    expression.push('NOT');
+                    break;
+                case 'AND':
+                    expression.push('AND');
+                    break;
+                default:
+                    throw new Error('Invalid boolean string');
+            }
         }
 
-        if (booleanString === 'TRUE') {
-            return [true];
-        }
-
-        if (booleanString === 'FALSE') {
-            return [false];
-        }
-
-        throw new Error('Invalid boolean string');
+        return expression;
     }
 }
