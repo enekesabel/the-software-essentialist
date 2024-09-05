d654739229bdb2c6a330178da545a0073dcb6b2d
enekesabel <enekes.abel@gmail.com>
2024-09-05 14:47:38 +0200

    refactor: parameterize tests

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -2,16 +2,14 @@ import { BooleanCalculator } from ".";
 
 describe('boolean calculator', () => {
 
-    it('Should evaluate "TRUE" to true', () => {
-        const expression = 'TRUE';
+    it.each([
+        { expression: 'TRUE', expected: true },
+        { expression: 'FALSE', expected: false },
+        { expression: 'NOT TRUE', expected: false },
+        { expression: 'NOT FALSE', expected: true },
+    ])('Should evaluate "$expression" correctly', ({ expression, expected }) => {
         const result = BooleanCalculator.Evaluate(expression);
-        expect(result).toBe(true);
-    });
-
-    it('Should evaluate "FALSE" to false', () => {
-        const expression = 'FALSE';
-        const result = BooleanCalculator.Evaluate(expression);
-        expect(result).toBe(false);
+        expect(result).toBe(expected);
     });
 
     it('Should throw an error for an invalid expression', () => {
@@ -20,12 +18,6 @@ describe('boolean calculator', () => {
         expect(() => {
             BooleanCalculator.Evaluate(expression);
         }).toThrowError("Invalid boolean expression");
-    });
-
-    it('Should evaluate "NOT TRUE" to false', () => {
-        const expression = 'NOT TRUE';
-        const result = BooleanCalculator.Evaluate(expression);
-        expect(result).toBe(false);
     });
 
 });
