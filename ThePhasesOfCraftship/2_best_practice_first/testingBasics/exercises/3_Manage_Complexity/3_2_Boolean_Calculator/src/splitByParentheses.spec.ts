4c0f491520e3d6bd1a3f5438ffd33b429f5cbfeb
enekesabel <enekes.abel@gmail.com>
2024-09-06 16:04:24 +0200

    feat: handle multiple parentheses

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/splitByParentheses.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/splitByParentheses.spec.ts
@@ -1,21 +1,39 @@
 describe('splitByParentheses', () => {
 
     const splitByParentheses = (str: string): string[] =>{
-        return str.split(')')
-        .map(s=>s.trim()
-        .replace('(', ''))
-        .filter(Boolean);
+
+        const result: string[] = [];
+        let openCount = 0;
+        for (let i = 0; i < str.length; i++) {
+            const char = str[i];
+            if(char === '(') {
+                if(openCount === 0) {
+                    result.push('');
+                }
+                openCount++;
+                continue;
+            } 
+            if (char === ')') {
+                openCount--;
+                if(openCount === 0 && i !== str.length - 1) {
+                    result.push('');
+                }
+                continue;
+            }
+            result[result.length - 1] += char;
+        }
+
+        return result.map(r=>r.trim());
     }
 
-    const testCases = [
+    it.each([
         { str: "(TRUE OR FALSE) AND TRUE", expectedResult: [
             'TRUE OR FALSE',
             'AND TRUE'
         ]},
         { str: "(TRUE OR FALSE)", expectedResult: ['TRUE OR FALSE']},
-    ];
-
-    it.each(testCases)('should split by parentheses $str', ({ str, expectedResult }) => {
+        { str: "(TRUE) OR (FALSE)", expectedResult: ['TRUE', 'OR', 'FALSE']},
+    ])('should split by parentheses $str', ({ str, expectedResult }) => {
         // act
         const result = splitByParentheses(str);
 
