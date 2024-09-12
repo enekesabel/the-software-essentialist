e9de18ef8a9d9dd4905419ad9a19701347a61670
enekesabel <enekes.abel@gmail.com>
2024-09-12 13:55:47 +0200

    refactor: extract class enrollemnts to ClassEnrollmentsController

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/controller/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/controller/index.ts
@@ -2,3 +2,4 @@ export { StudentsController } from './StudentsController';
 export { ClassesController } from './ClassesController';
 export { StudentAssignmentsController } from './StudentAssignmentsController';
 export { AssignmentsController } from './AssignmentsController';
+export { ClassEnrollmentsController } from './ClassEnrollmentsController';
--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/index.ts
@@ -1,8 +1,6 @@
-import express, { Request, Response } from 'express';
-import { prisma } from './database';
-import { Errors } from './controller/Errors';
-import { isMissingKeys, parseForResponse, isUUID } from './controller/utils';
-import { StudentsController, ClassesController, StudentAssignmentsController, AssignmentsController } from './controller';
+import express from 'express';
+import { StudentsController, ClassesController, StudentAssignmentsController, AssignmentsController, ClassEnrollmentsController } from './controller';
+
 const cors = require('cors');
 const app = express();
 app.use(express.json());
@@ -22,62 +20,8 @@ app.use('/student-assignments', studentAssignmentsController.router);
 const assignmentsController = new AssignmentsController();
 app.use('/assignments', assignmentsController.router);
 
-// POST student assigned to class
-app.post('/class-enrollments', async (req: Request, res: Response) => {
-    try {
-        if (isMissingKeys(req.body, ['studentId', 'classId'])) {
-            return res.status(400).json({ error: Errors.ValidationError, data: undefined, success: false });
-        }
-    
-        const { studentId, classId } = req.body;
-    
-        // check if student exists
-        const student = await prisma.student.findUnique({
-            where: {
-                id: studentId
-            }
-        });
-    
-        if (!student) {
-            return res.status(404).json({ error: Errors.StudentNotFound, data: undefined, success: false });
-        }
-    
-        // check if class exists
-        const cls = await prisma.class.findUnique({
-            where: {
-                id: classId
-            }
-        });
-
-        // check if student is already enrolled in class
-        const duplicatedClassEnrollment = await prisma.classEnrollment.findFirst({
-            where: {
-                studentId,
-                classId
-            }
-        });
-
-        if (duplicatedClassEnrollment) {
-            return res.status(400).json({ error: Errors.StudentAlreadyEnrolled, data: undefined, success: false });
-        }
-    
-        if (!cls) {
-            return res.status(404).json({ error: Errors.ClassNotFound, data: undefined, success: false });
-        }
-    
-        const classEnrollment = await prisma.classEnrollment.create({
-            data: {
-                studentId,
-                classId
-            }
-        });
-    
-        res.status(201).json({ error: undefined, data: parseForResponse(classEnrollment), success: true });
-    } catch (error) {
-        res.status(500).json({ error: Errors.ServerError, data: undefined, success: false });
-    }
- 
-});
+const classEnrollmentsController = new ClassEnrollmentsController();
+app.use('/class-enrollments', classEnrollmentsController.router);
 
 const port = process.env.PORT || 3000;
 
