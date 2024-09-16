c76f48eb80428ac59b491f8601e39aa062d13242
enekesabel <enekes.abel@gmail.com>
2024-09-16 10:47:00 +0200

    fix: extend DTOValidationEroor from base error

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/dto/utils.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/dto/utils.ts
@@ -1,4 +1,6 @@
-export class DTOValidationError extends Error {}
+import { BaseError } from "../Errors";
+
+export class DTOValidationError extends BaseError {}
 
 export function isInvalidDTO<T>(dto: any): dto is DTOValidationError {
     return dto instanceof DTOValidationError;
