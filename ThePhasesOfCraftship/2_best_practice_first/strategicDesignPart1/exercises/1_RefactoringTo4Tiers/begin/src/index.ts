010dc3eb95e7ffc3b4249e1b34604ed81d391b33
enekesabel <enekes.abel@gmail.com>
2024-09-17 14:22:03 +0200

    refactor: extract bootstrapping app

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/index.ts
@@ -1,47 +1,7 @@
-import express from 'express';
-import { StudentsController, ClassesController, StudentAssignmentsController, AssignmentsController, ClassEnrollmentsController } from './controller';
-import { AssignmentsService, ClassEnrollmentsService, ClassesService, StudentAssignmentsService, StudentsService } from './service';
-import { errorHandler } from './middleware';
-import { AssingmentsRepository, ClassEnrollmentsRepository, ClassesRepository, StudentAssignmentsRepository, StudentsRepository } from './persistence';
-import { prisma } from './database';
+import { Server } from "./bootstrap";
 
-const cors = require('cors');
-const app = express();
-app.use(express.json());
-app.use(cors());
+const port = Number(process.env.PORT || 3000);
 
-// Persistence
-const assignmentsRepository = new AssingmentsRepository(prisma);
-const classesRepository = new ClassesRepository(prisma);
-const classEnrollmentsRepository = new ClassEnrollmentsRepository(prisma);
-const studentsRepository = new StudentsRepository(prisma);
-const studentAssignmentsRepository = new StudentAssignmentsRepository(prisma);
+const server = new Server();
 
-// Services
-const studentsService = new StudentsService(studentsRepository, studentAssignmentsRepository);
-const classesService = new ClassesService(assignmentsRepository, classesRepository);
-const studentAssignmentsService = new StudentAssignmentsService(assignmentsRepository, studentsRepository, studentAssignmentsRepository);
-const assignmentsService = new AssignmentsService(assignmentsRepository);
-const classEnrollmentsService = new ClassEnrollmentsService(classesRepository, classEnrollmentsRepository, studentsRepository);
-
-// Controllers
-const studentsController = new StudentsController(studentsService);
-const classesController = new ClassesController(classesService);
-const studentAssignmentsController = new StudentAssignmentsController(studentAssignmentsService);
-const assignmentsController = new AssignmentsController(assignmentsService);
-const classEnrollmentsController = new ClassEnrollmentsController(classEnrollmentsService);
-
-// API Endpoints
-app.use('/students', studentsController.router);
-app.use('/classes', classesController.router);
-app.use('/student-assignments', studentAssignmentsController.router);
-app.use('/assignments', assignmentsController.router);
-app.use('/class-enrollments', classEnrollmentsController.router);
-
-app.use(errorHandler);
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+server.start(port);
